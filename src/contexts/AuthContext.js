96c8db40c7018cf420ab32b231ebe4a29b8debad
Gilna <meethalgilna@gmail.com>
2021-11-02 01:35:32 +0530

    first commit

--- src/contexts/AuthContext.js
+++ src/contexts/AuthContext.js
@@ -1,5 +1,7 @@
 import React, { useContext, useState, useEffect } from "react"
-import { auth } from "../firebase"
+import { auth,firestore} from "../firebase"
+import { updateDoc, setDoc, doc, getDoc } from "firebase/firestore";
+import {  FacebookAuthProvider, signInWithPopup, signOut, signInWithEmailAndPassword}  from "firebase/auth";
 
 const AuthContext = React.createContext()
 
@@ -10,6 +12,115 @@ export function useAuth() {
 export function AuthProvider({ children }) {
   const [currentUser, setCurrentUser] = useState()
   const [loading, setLoading] = useState(true)
+  const [authError, setAuthError] = useState("");
+  const [userInfo, setUserInfo] = useState({});
+
+  const signInWithFacebook = async () => {
+    console.log(FacebookAuthProvider)
+    const provider = new FacebookAuthProvider();
+    try {
+        const result = await signInWithPopup(auth, provider)
+        const user = result.user
+
+        //Realtime Database functions
+        // const dbRef = ref(db)
+        // const snapshot = await get(child(dbRef, `users/${user.uid}`))
+        // if (!snapshot.exists()) {
+        //     try {
+        //         await set(ref(db, 'users/' + user.uid), {
+        //             username: user.displayName,
+        //             email: user.email,
+        //             profile_picture: user.photoURL,
+        //             provider: "google"
+        //         });
+
+        //     } catch (err) {
+        //         console.log(err)
+        //     }
+        // }
+
+        try {
+            await setDoc(doc(firestore, "users", user.uid), {
+                username: user.displayName,
+                email: user.email,
+                profile_picture: user.photoURL,
+                provider: "facebook"
+            });
+        } catch (e) {
+            console.error("Error adding document: ", e);
+        }
+        setCurrentUser(user)
+    } catch (err) {
+        setAuthError(err.message)
+    }
+
+}
+const logInWithEmail = async (email, password) => {
+  try {
+      const userCredential = await signInWithEmailAndPassword(auth, email, password)
+      const user = userCredential.user;
+      console.log(user)
+  }
+  catch (error) {
+      const errorMessage = error.message;
+      setAuthError(errorMessage)
+      setCurrentUser(null)
+  }
+}
+
+const logOut = () => {
+  signOut(auth).then(() => {
+      setCurrentUser(null)
+  }).catch((error) => {
+      setAuthError(error)
+  });
+}
+
+const updateUserInfo = async (name, address, age) => {
+  try {
+
+      //Realtime Database functions
+      // await update(ref(db, 'users/' + currentUser.uid), {
+      //     full_name: name,
+      //     address: address,
+      //     age: age
+      // });
+
+      await updateDoc(doc(firestore, "users", currentUser.uid), {
+          full_name: name,
+          address: address,
+          age: age
+      });
+  } catch (err) {
+      setAuthError(err)
+  }
+}
+
+const getUserInfo = async () => {
+
+  //Realtime Database functions
+  // const dbRef = ref(db)
+  // get(child(dbRef, `users/${currentUser.uid}`)).then((snapshot) => {
+  //     if (snapshot.exists()) {
+  //         setUserInfo(snapshot.val());
+  //     } else {
+  //         console.log("No data available");
+  //     }
+  // }).catch((error) => {
+  //     console.error(error);
+  // });
+
+  const docRef = doc(firestore, "users", currentUser.uid);
+  const docSnap = await getDoc(docRef);
+  if (docSnap.exists()) {
+      setUserInfo(docSnap.data())
+  } else {
+      // doc.data() will be undefined in this case
+      setAuthError("No user")
+  }
+}
+
+
 
   function signup(email, password) {
     return auth.createUserWithEmailAndPassword(email, password)
@@ -51,7 +162,15 @@ export function AuthProvider({ children }) {
     logout,
     resetPassword,
     updateEmail,
-    updatePassword
+    logInWithEmail,
+    logOut,
+    updatePassword,
+    signInWithFacebook,
+    updateUserInfo,
+    getUserInfo,
+    authError,
+    userInfo
+
   }
 
   return (
--- src/firebase.js
+++ src/firebase.js
@@ -1,15 +1,20 @@
-import firebase from "firebase/app"
-import "firebase/auth"
+import {initializeApp} from "firebase/app"
+import {getAuth} from "firebase/auth"
+import { getDatabase } from 'firebase/database';
+import {getFirestore} from 'firebase/firestore'
 
-const app = firebase.initializeApp({
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID
-})
+const firebaseConfig= {
+  apiKey: "AIzaSyBABoRsC8uGPgQWCTRSP5aEnTJI3ciUEvY",
+  authDomain: "internship-b513f.firebaseapp.com",
+  projectId: "internship-b513f",
+  storageBucket: "internship-b513f.appspot.com",
+  messagingSenderId: "534926851092",
+  appId: "1:534926851092:web:c6cc137a4a08a16ea3fb01"
+}
 
-export const auth = app.auth()
+const app = initializeApp(firebaseConfig);
+
 export default app
+export const auth = getAuth();
+export const db = getDatabase();
+export const firestore = getFirestore();
